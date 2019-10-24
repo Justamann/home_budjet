e0e81554f675f5a033baf6fc344eab5c685bf13a
Evgeniy Agafonov <e.agafonov@itgene.ru>
2019-10-24 14:13:45 +0200

    set static folder

--- home_budjet/settings.py
+++ home_budjet/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'Europe-Moscow'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
